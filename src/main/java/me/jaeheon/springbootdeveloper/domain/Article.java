2d23283b9d82e9a526981f5773a9ee981dbbab52
2jaeheon <jaeheon0826@jbnu.ac.kr>
2024-12-31 22:17:20 +0900

    Revert "ADD: Blog Article CRUD"
    This reverts commit 50cf4aa9cc3dfc64627fe01e0d944855cb544697.

--- src/main/java/me/jaeheon/springbootdeveloper/domain/Article.java
+++ src/main/java/me/jaeheon/springbootdeveloper/domain/Article.java
@@ -2,22 +2,16 @@ package me.jaeheon.springbootdeveloper.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity // 엔티티로 지정 -> DB 테이블과 매핑됨.
 @Getter // Getter를 자동으로 생성
-@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자를 자동으로 생성
 // 기본 생성자의 접근제어자를 AccessLevel로 설정함.
 public class Article {
@@ -36,15 +30,6 @@ public class Article {
     @Column(name = "content")
     private String content;
 
-    // CreatedDate를 사용하면 엔티티가 생성될 때 created_at 컬럼에 저장
-    @CreatedDate
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-
     // 빌더 패턴으로 객체 생성 (Lombok)
     // 빌더 패턴을 사용하면 객체를 유연하고 직관적으로 생성이 가능
     // 빌더 패턴을 사용하면 어느 필드에 어떤 값이 들어가는지 명시적 파악이 가능함.
@@ -59,6 +44,4 @@ public class Article {
         this.title = title;
         this.content = content;
     }
-
-
 }
